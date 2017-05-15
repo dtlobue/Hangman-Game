1dd45ec31de2f1daf6a4067380b0297ff4367954
Daniel LoBue <dlobue@Daniels-MacBook-Pro.local>
2017-05-14 19:44:03 -0700

    think I figured out pseudocode reference problem, added some js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,21 @@
 
 			console.log(numBlanks);
 
+			// reset the guess and success array at each round. 
+		    // Array of letters (first array, for succesful guesses)
+
+            // reset the wrong guesses from the previous round. 
+            // Array of letters (second arrays, one for fails)
+
+            var blanksAndSuccesses = [];
+  				for (var i = 0; i < numBlanks; i++) {
+    			   blanksAndSuccesses.push("_");
+ 						 }
+            // here call document to update spaces 
+            document.getElementById('word-blanks').innerHTML = blanksAndSuccesses.join('');
+
 		}
+
+		
 
 
